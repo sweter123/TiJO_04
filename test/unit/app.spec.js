f49b22708d55721bd98d55d7746f44038a986672
sweter123 <bortekwalaszak69@gmail.com>
2016-03-20 15:54:21 +0100

    added unit tests

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -35,4 +35,69 @@ describe('app', function () {
             expect(app.divide(101, 0)).toEqual(false);
         });
     });
+    describe('getDescendingNumbers function', function () {
+        it('should return "a a-1 ... b" when a > b', function () {
+            expect(app.getDescendingNumbers(8, 2)).toEqual('8 7 6 5 4 3 2');
+        });
+        it('should return false when b > a', function () {
+            expect(app.getDescendingNumbers(2, 8)).toEqual(false);
+        });
+        it('should return false when a is not number', function () {
+            expect(app.getDescendingNumbers("s", 8)).toEqual(false);
+        });
+        it('should return false when b is not number', function () {
+            expect(app.getDescendingNumbers(2, "s")).toEqual(false);
+        });
+    });
+    describe('areaOfTrapezoid function', function () {
+        it('should return area of trapezoid if a < 0 && b < 0 && c < 0', function () {
+            expect(app.areaOfTrapezoid(8, 2, 3)).toEqual(15);
+        });
+        it('should return false if c < 0', function () {
+            expect(app.areaOfTrapezoid(3, 5, -1)).toEqual(false);
+        });
+        it('should return false if b < 0', function () {
+            expect(app.areaOfTrapezoid(3, -5, 1)).toEqual(false);
+        })
+        it('should return false if a < 0', function () {
+            expect(app.areaOfTrapezoid(-3, 5, 1)).toEqual(false);
+        })
+        it('should return false if b is not number', function () {
+            expect(app.areaOfTrapezoid(3, "g", 1)).toEqual(false);
+        })
+        it('should return false if c is not number', function () {
+            expect(app.areaOfTrapezoid(3, 5, "a")).toEqual(false);
+        })
+        it('should return false if a is not number', function () {
+            expect(app.areaOfTrapezoid("a", 5, 1)).toEqual(false);
+        })
+    });
+    describe('maxArray function', function () {
+        it('should return max of array if array contains numbers', function () {
+            expect(app.maxArray([1,2,3,4,5,8])).toEqual(8);
+        });
+        it('should return false if contains invalid characters', function () {
+            expect(app.maxArray([1,2,3,"b",6,7,8])).toEqual(false);
+        });
+        it('should return max of array if array contains only one element', function () {
+            expect(app.maxArray([1])).toEqual(1);
+        });
+        it('should return max of array if array contains few same elements', function () {
+            expect(app.maxArray([3,3,3,3,3,3,3])).toEqual(3);
+        });
+    });
+    describe('squareOdd', function () {
+        it('should return array with squared odd elements with unchanged even numbers if array contains odd numbers and even numbers', function () {
+            expect(app.squareOdd([1,2,3,4,5,8])).toEqual([1,2,9,4,25,8]);
+        });
+        it('should return array with squared odd elements if array contains only odd numbers', function () {
+            expect(app.squareOdd([1,3,5,7,9,11])).toEqual([1,9,25,49,81,121]);
+        });
+        it('should return array with squared odd elements if array contains odd numbers with unchanged string elements', function () {
+            expect(app.squareOdd([1,3,"g",7,9,11])).toEqual([1,9,"g",49,81,121]);
+        });
+        it('should return false if array contains elements other than numbers or strings', function () {
+            expect(app.squareOdd([1,3,"g",7,true,11])).toEqual(false);
+        });
+    });
 });
